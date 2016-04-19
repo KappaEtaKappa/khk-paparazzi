9e6e9d1a64c5542f58b102a3ca3dd6c82f2c1e7a
Joseph Dailey <josephldailey@gmail.com>
2016-04-19 10:28:19 -0500

    scp and job management

--- dump.js
+++ dump.js
@@ -1,5 +1,8 @@
 var fs = require('fs');
 
+var util = require('util');
+var exec = require('child_process').exec;
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -42,6 +45,25 @@ function removeUpload(filename, token){
     }
   }
 }
+var currentJobs;
+var maxJobs = 10;
+var jobCounter;
+
+function checkThenRun(cmd, job){
+  if(currentJobs >= maxJobs){
+    console.log("Warning: Max number of current jobs met, waiting 200ms");
+    setTimeout(function(){checkThenRun(cmd, job);}, 200);
+  }else{
+    currentJobs++;
+    var child = exec(cmd, function(error, stdout, stderr){ 
+      if(error) console.log('Job:'+job+' Error: ' + error);
+      else console.log('Job:'+job+' Synced! ' + stdout);
+      currentJobs--;
+    });
+  }
+}
+
+
 app.post("/upload", upload.array("photos", 50), function(req, res){
   var token = req.body.token;
   console.log(token);
@@ -52,15 +74,19 @@ app.post("/upload", upload.array("photos", 50), function(req, res){
   erroredUploads[token] = [];
 
   req.files.forEach(function(file){
-    console.log(file.originalname.split(".")[file.originalname.split(".").length-1])
     if(file.mimetype.split("/")[0] == "image"){
-      fs.rename(file.path, "public/images/" + file.filename + "." +file.originalname.split(".")[file.originalname.split(".").length-1], function(err){
+      var newPath = __dirname + "/public/images/" + file.filename + "." +file.originalname.split(".")[file.originalname.split(".").length-1]
+      console.log(newPath)
+      fs.rename(file.path, newPath, function(err){
         if(err){
           erroredUploads[token].push(file.originalname);
           removeUpload(file.filename, token);
           console.log(err);
         }else{
           removeUpload(file.filename, token);
+          cmd = 'scp -pr "' + newPath + '" ' + 'khk@10.0.0.10:"' + "/home/admin/photos/_Current (Spring 2016)/unsorted".replace(/(["\s'$`\\()])/g,'\\$1') + '"';
+          console.log('Job:'+job+'\('+currentJobs+'//'+maxJobs+'\) Add/Sync: ', cmd);
+          checkThenRun(cmd, ++jobCounter);
         }
       });
       
