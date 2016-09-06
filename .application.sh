7622eff471def7fdcc8903e35d40e7ab4088e941
Joseph Dailey <josephldialey@gmail.com>
2016-09-06 04:31:58 +0000

    more fixes for khk-web deployment

--- .application.sh
+++ .application.sh
@@ -1,1 +1,2 @@
-sqlite3 ../khk-ssa/khk-access/db.sqlite "INSERT INTO apps (app_id, name, privilegeRequired, subdomain, icon) values (3, \"Paparazzi\", 1, \"paparazzi\", \"fa-camera-retro\");"
+#!/bin/bash
+sqlite3 ../khk-ssa/khk-access/db.sqlite "INSERT INTO apps (name, privilegeRequired, subdomain, icon) values (\"Paparazzi\", 1, \"paparazzi\", \"fa-camera-retro\");"
